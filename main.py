8dcbbebbee528d85bf44affd652facde9b37fca9
Vebnik(Nik) <vebnik111@gmail.com>
2023-01-01 22:50:47 +0300

    add ytdl

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # libs
 from PyQt6.QtWidgets import QMainWindow, QWidget, QApplication
-from PyQt6 import uic
+import youtube_dl, json
 
 # module
 from src.pyqt.app_ui import Ui_MainWindow
@@ -16,12 +16,25 @@ def main():
       self.main_ui = Ui_MainWindow()
       self.main_ui.setupUi(self)
 
+      # TODO декомпозировать нвешивание хендлеров
       self.main_ui.configBtn_4.clicked.connect(self.on_click_download)
       self.main_ui.configBtn_2.clicked.connect(self.on_click_info)
       self.main_ui.configBtn_3.clicked.connect(self.on_click_save)
 
+    # TODO вынести хендлеры в отдельный класс
     # collbacl-handlers-action
     def on_click_info(self, *args) -> None:
+      video_url = self.main_ui.videoUrl.text()
+
+      if video_url:
+        # TODO для ytdl придумать враппер-логику
+        with youtube_dl.YoutubeDL() as ytdl:
+          info = ytdl.extract_info(video_url, download=False)
+
+          self.main_ui.lineEdit_2.setText(info.get('title'))
+          self.main_ui.lineEdit_4.setText(str(info.get('duration')))
+          self.main_ui.lineEdit_3.setText(info.get('channel'))
+
       print('click on_click_info', args)
 
     def on_click_download(self, *args) -> None:
